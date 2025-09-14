5ee8a9a8d90b18a011bc381fdf53b452c19b9b6f
Pehan Januka <pehanjanu@gmail.com>
2025-09-14 23:40:49 +0530

    Modify the AboutSection

--- pehan-portfolio/src/components/AboutSection.jsx
+++ pehan-portfolio/src/components/AboutSection.jsx
@@ -14,19 +14,21 @@ export const AboutSection = () => {
             <h3 className="text-2xl font-semibold"> Passionate Full Stack Developer</h3>
             
             <p className="text-muted-foreground">
-----------------------------------------------------
+              I am a dedicated second year Computer Science undergraduate with a strong passion for Full Stack Development. I enjoy creating dynamic, user friendly web applications that solve real world problems and deliver meaningful experiences. With hands on experience in frontend and backend technologies, including the MERN stack, Java, and cloud fundamentals like AWS, I continuously explore modern tools and frameworks to enhance my skills.
+              
             </p>
 
             <p className="text-muted-foreground">
-              ----------------------
+                I thrive in collaborative environments and enjoy working on projects that challenge my problem solving abilities. Currently, I am also learning about AI and other emerging technologies to stay ahead in the fast paced tech industry. My goal is to leverage my knowledge and creativity to build innovative solutions that make a positive impact in the digital world while growing as a skilled Full Stack Developer
             </p>
+
             <div className=" flex flex-col sm:flex-row gap-4 pt-4 justify-center"> 
               <a href="#contact" className="cosmic-button">
                 {" "}
                 Get In Touch
                  </a>
 
-                 <a href="" className="px-6 py-2 rounded-full border border-primary text-primary  hover:bg-primary/10 transition-color duration-300">
+                 <a href="/Pehan_Gunawardana_Resume.pdf" className="px-6 py-2 rounded-full border border-primary text-primary  hover:bg-primary/10 transition-color duration-300">
                  Download CV
                  </a>
 
@@ -55,9 +57,9 @@ export const AboutSection = () => {
                     <User  className="h-6 w-6 text-primary"/>
                   </div>
                    <div className="text-left">
-                    <h4 className="font-semibold text-lg">Mobile Application Development</h4>
+                    <h4 className="font-semibold text-lg">UI/UX Design & Prototyping</h4>
                     <p className="text-muted-foreground ">
-                   Exploring intelligent solutions with data-driven models to solve real-world problems and create smarter applications.
+                        Designing user-friendly interfaces and experiences, creating interactive prototypes to visualize and test ideas before development
                     </p>
                   </div>
                 </div>   
